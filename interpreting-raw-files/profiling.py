633cee655be116f81425e86b58ec5137254a322c
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-04-12 09:02:51 -0400

    Profiling update to be more accurate.

--- interpreting-raw-files/profiling.py
+++ interpreting-raw-files/profiling.py
@@ -11,8 +11,6 @@ from pycallgraph2 import Config
 from pycallgraph2 import GlobbingFilter
 from pycallgraph2.output import GraphvizOutput
 
-from GENERATE_DATABASE import main
-
 
 config = Config()
 config.trace_filter = GlobbingFilter(exclude=[
@@ -22,4 +20,5 @@ config.trace_filter = GlobbingFilter(exclude=[
 graphviz = GraphvizOutput(output_file='call-graph.png')
 
 with PyCallGraph(output=graphviz, config=config):
+    from GENERATE_DATABASE import main
     main()
